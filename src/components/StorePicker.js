9ea6963c07be7c2e168a11010804cabebc44174c
Tyler Ouellette <ouell117@uwindsor.ca>
2020-01-23 16:59:46 -0500

    starting on submit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,21 @@
 import React, { Component, Fragment } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+    onSubmit = e => {
+        e.preventDefault();
+    };
+
+    onChange = e => {
+        
+    };
+
     render() {
         return (
             <Fragment>
-                <form action="" className="store-selector">
+                <form action="" className="store-selector" onSubmit={this.onSubmit}>
                     <h2>Please Enter a Store</h2>
-                    <input type="text" name="" id="" required placeholder="Store Name" />
+                    <input type="text" name="" id="" required placeholder="Store Name" defaultValue={getFunName()} />
                     <button type="submit">Visit Store --></button>
                 </form>
             </Fragment>
