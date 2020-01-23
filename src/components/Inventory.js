4a0c684af7ba6a0c79d0932824685db61dc25d0e
Tyler Ouellette <ouell117@uwindsor.ca>
2020-01-23 14:41:38 -0500

    header, inv, order components

---
+++ src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class Inventory extends Component {
+    render() {
+        return (
+            <Fragment>
+              <div className="inventory">Inventory</div>
+            </Fragment>
+        );
+    }
+}
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class Order extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="order">Orders</div>
+                
+            </Fragment>
+        );
+    }
+}
+
+export default Order;
