54c6b56f87060fa82218fc9c8fe3d71771d8bd87
Tyler Ouellette <ouell117@uwindsor.ca>
2020-03-08 21:07:57 -0400

    vid 14

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends Component {
     render() {
         return (
             <Fragment>
-              <div className="inventory">Inventory</div>
+                <div className="inventory">
+                    <h2>Inventory</h2>
+                    <AddFishForm addFish={this.props.addFish} />
+                </div>
             </Fragment>
         );
     }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,20 +2,28 @@ import React, { Component, Fragment } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+    myInput = React.createRef();
+
     onSubmit = e => {
         e.preventDefault();
+        const storeName = this.myInput.value;
+        this.props.history.push(`/store/${storeName}`);
     };
 
-    onChange = e => {
-        
-    };
+    onChange = e => {};
 
     render() {
         return (
             <Fragment>
                 <form action="" className="store-selector" onSubmit={this.onSubmit}>
                     <h2>Please Enter a Store</h2>
-                    <input type="text" name="" id="" required placeholder="Store Name" defaultValue={getFunName()} />
+                    <input
+                        type="text"
+                        ref={this.myInput}
+                        required
+                        placeholder="Store Name"
+                        defaultValue={getFunName()}
+                    />
                     <button type="submit">Visit Store --></button>
                 </form>
             </Fragment>
